dbe638144a03f877264567df7fb641a5b9b63d87
hassaan <hassaans299@gmail.com>
2022-12-06 00:27:29 +0500

    single page

--- server.mjs
+++ server.mjs
@@ -15,6 +15,8 @@ app.get('/abc', (req, res) => {
 const __dirname= path.resolve();
 
 app.use('/', express.static ( path.join(__dirname, './web/build') ) )
+app.use('*', express.static ( path.join(__dirname, './web/build') ) )
+
 
 
 app.listen(port, () => {
