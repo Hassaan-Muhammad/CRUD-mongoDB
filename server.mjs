66cc308d7ad04fde74ef69338c257f18889e22f8
hassaan <hassaans299@gmail.com>
2022-12-11 21:22:47 +0500

    CRUD-1

--- server.mjs
+++ server.mjs
@@ -5,34 +5,143 @@ import path from 'path';
 import cors from 'cors';
 
 const app = express()
-const port = process.env.PORT ||5003
+const port = process.env.PORT || 5003
 
 app.use(cors());
+app.use(express.json());
 
 
-app.get('/abc', (req, res) => {
+let products = [];
 
- console.log("Request ip", req.ip)   
-  res.send('Hello World! new ' + new Date().toString())
+app.post('/product', (req, res) => {
+
+  const body = req.body;
+
+  if (
+    !body.name
+    || !body.price
+    || !body.description
+  ) {
+    res.status(400).send( { message:"required parameter failed" })
+    return;
+  }
+
+  console.log(body.name)
+  console.log(body.price)
+  console.log(body.description)
+
+  products.push({
+    id: new Date().getTime(),
+    name: body.name,
+    price: body.price,
+    description: body.description
+  })
+
+  res.send( {  message: "Product succesfully stored"})
+
 })
 
-app.get('/weather', (req, res) => {
+app.get('/products', (req, res) => {
+  res.send( {  message:"Here are all products" ,
+              data: products})
+})
 
-    console.log("Request ip", req.ip)   
-   
-     res.send({ 
-        temp: 41,
-        min:51,
-        max:84,
-        humidity: 25,
-        serverTime: new Date().toString()
-      } )
-   })
+app.get('/product/:id', (req, res) => {
 
-const __dirname= path.resolve();
+  const id = req.params.id;
+  let isFound = false
 
-app.use('/', express.static ( path.join(__dirname, './web/build') ) )
-app.use('*', express.static ( path.join(__dirname, './web/build') ) )
+  for (let i = 0; i < products.length; i++) {
+    if (products[i].id == id) {
+
+      res.send( {  message:"Get product by id",
+                  data: products[i]})
+
+      isFound = true
+      break;
+    }
+  }
+
+  if (isFound == false) {
+    res.status(404).send( {  message:"product not found"})
+  }
+
+})
+
+app.delete('/product/:id', (req, res) => {
+
+  const id = req.params.id;
+  let isFound = false
+
+  for (let i = 0; i < products.length; i++) {
+    if (products[i].id == id) {
+
+      products.splice(i, 1)
+
+      res.send({  message:"Product deleted Successfuly"})
+      isFound = true
+      break;
+    }
+  }
+
+  if (isFound == false) {
+    res.status(404).send({  message:"Delete failed: product not found"})
+  }
+
+})
+
+app.put('/product/:id', (req, res) => {
+
+  const body = req.body;
+  const id = req.params.id;
+  let isFound = false
+
+  if (
+    !body.name
+    || !body.price
+    || !body.description
+  ) {
+    res.status(400).send({  message:"required parameter failed"})
+    return;
+  }
+
+  console.log(body.name)
+  console.log(body.price)
+  console.log(body.description)
+
+  for (let i = 0; i < products.length; i++) {
+    if (products[i].id == id) {
+
+      products[i].name= body.name;
+      products[i].price= body.price;
+      products[i].description= body.description;
+
+
+      res.send({  message: "Product modified Successfuly"})
+      isFound = true
+      break;
+    }
+  }
+
+  if (!isFound) {
+    res.status(404).send({  message:"Delete failed: product not found"})
+  }
+
+  res.send({  message:"Product succesfully stored"})
+
+})
+
+
+
+
+
+
+
+
+const __dirname = path.resolve();
+
+app.use('/', express.static(path.join(__dirname, './web/build')))
+app.use('*', express.static(path.join(__dirname, './web/build')))
 
 
 
